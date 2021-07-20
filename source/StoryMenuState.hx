ed49ec972a6c1c4173e1541aa0e706fd6457738f
gemgames <legomastersform@gmail.com>
2021-07-20 14:35:22 -0400

    Changed to "dad"

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -34,7 +34,7 @@ class StoryMenuState extends MusicBeatState
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
-		['jetrack', 'bf', 'gf']
+		['dad', 'bf', 'gf']
 	];
 
 	var weekNames:Array<String> = [
